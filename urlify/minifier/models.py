449f6ccf22706aad6083a04b2da3c4d2e28061c0
Christopher Adams <christopher.r.adams@gmail.com>
2013-08-31 17:37:47 -0400

    Added relation to auth.user at 'submitter' field.

--- urlify/minifier/models.py
+++ urlify/minifier/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class MinifiedURL(models.Model):
+    submitter = models.ForeignKey('auth.user', null=True)
     url = models.CharField(max_length=100)
     datetime = models.DateTimeField(auto_now_add=True)
 
