2b3bece0058f70c987afafb9dabe6a710cd021c2
Christopher Adams <christopher.r.adams@gmail.com>
2013-08-31 18:37:06 -0400

    Add field 'updated'.

--- urlify/minifier/models.py
+++ urlify/minifier/models.py
@@ -5,3 +5,4 @@ class MinifiedURL(models.Model):
     submitter = models.ForeignKey('auth.user', null=True)
     url = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
