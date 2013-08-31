dea2fb0c5cb1e53acae2e28871da8b3510c66557
Christopher Adams <christopher.r.adams@gmail.com>
2013-08-31 18:27:52 -0400

    Data migration from the ''datetime' to 'created' field.

--- urlify/minifier/models.py
+++ urlify/minifier/models.py
@@ -4,5 +4,4 @@ from django.db import models
 class MinifiedURL(models.Model):
     submitter = models.ForeignKey('auth.user', null=True)
     url = models.CharField(max_length=100)
-    datetime = models.DateTimeField(auto_now_add=True)
-
+    created = models.DateTimeField(auto_now_add=True)
