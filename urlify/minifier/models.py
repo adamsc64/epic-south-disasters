728cfb5410e5230b5e41d6f8fb4f7d6be8d31d5e
Christopher Adams <christopher.r.adams@gmail.com>
2013-08-31 22:47:17 -0400

    Fake sending a user email by handling the post_save signal.

--- urlify/minifier/models.py
+++ urlify/minifier/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class MinifiedURL(models.Model):
@@ -7,3 +9,18 @@ class MinifiedURL(models.Model):
     domain = models.CharField(max_length=30)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save)
+def email_user_on_url_change(sender, **kwargs):
+    if sender == MinifiedURL:
+        user = kwargs['instance'].submitter
+        if user:
+            email(user,
+                "Congratulations on changing "
+                "the URL, %s!" % user.username
+                )
+
+def email(user, body):
+    """Fakes sending an email for demo purposes."""
+    print "%s: %s" % (user, body)
