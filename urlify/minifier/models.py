21a989d029704c12d8c7abab13d9abcc44ee768b
Christopher Adams <christopher.r.adams@gmail.com>
2013-08-31 20:12:46 -0400

    Add new field for 'domain'.

--- urlify/minifier/models.py
+++ urlify/minifier/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class MinifiedURL(models.Model):
     submitter = models.ForeignKey('auth.user', null=True)
     url = models.CharField(max_length=100)
+    domain = models.CharField(max_length=30)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
